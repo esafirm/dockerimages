6c74d8d5dd87b4bc073d7dbe1125d660652af528
Esa Firman <esafirm21@gmail.com>
2019-12-07 06:48:32 +0700

    Fix the condition

--- alpine-android/init.sh
+++ alpine-android/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z $DBX_AUTH_TOKEN ]]; then 
+if [[ ! -z $DBX_AUTH_TOKEN ]]; then 
     mkdir -p $HOME/.config/dbxcli
     echo "{\"\":{\"personal\":\"$DBX_AUTH_TOKEN\"}}" > $HOME/.config/dbxcli/auth.json
 else
-    echo "Do DBX auth token provided!"
+    echo "No DBX auth token provided!"
 fi
 
