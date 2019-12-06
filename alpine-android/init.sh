40e7598c4aadd92d181bb2ef70e0cbc4bff4dc15
Esa Firman <esafirm21@gmail.com>
2019-12-06 17:56:44 +0700

    More friendly init

--- alpine-android/init.sh
+++ alpine-android/init.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/.config/dbxcli
-echo "{\"\":{\"personal\":\"$DBX_AUTH_TOKEN\"}}" > $HOME/.config/dbxcli/auth.json
+if [[ -z $DBX_AUTH_TOKEN ]]; then 
+    mkdir -p $HOME/.config/dbxcli
+    echo "{\"\":{\"personal\":\"$DBX_AUTH_TOKEN\"}}" > $HOME/.config/dbxcli/auth.json
+else
+    echo "Do DBX auth token provided!"
+fi
+
