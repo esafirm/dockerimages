518e1936f7ae72c0fd0b58c9b72d584f3d0c0ba3
Esa Firman <esafirm21@gmail.com>
2019-12-23 10:38:48 +0700

    add setup script

---
+++ alpine-android/setup.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+GITHUB_BASE="https://github.com/esafirm/dockerimages/blob/master/alpine-android/bin"
+
+DBX_LINUX_LINK="$GITHUB_BASE/linux/dbxcli?raw=true"
+GDRIVE_LINUX_LINK="$GITHUB_BASE/linux/gdrive?raw=true"
+
+DBX_OSX_LINK="$GITHUB_BASE/osx/dbxcli?raw=true"
+GDRIVE_OSX_LINK="$GITHUB_BASE/osx/gdrive?raw=true"
+
+DBX_DEST=usr/local/bin/dbxcli
+GDRIVE_DEST=usr/local/bin/gdrive
+
+installDbx() {
+    LINK=$1
+    if [[ $NEED_DBX ]]; then
+        curl -sL $LINK > $DBX_DEST
+        chmod +x $DBX_DEST
+    fi
+}
+
+installGdrive() {
+    LINK=$1
+    if [[ $NEED_GDRIVE ]]; then
+        curl -sL $LINK > $GDRIVE_DEST
+        chmod +x $GDRIVE_DEST
+    fi
+}
+
+OS=$(uname)
+
+if [[ $(command -v dbxcli) ]]; then
+    NEED_DBX=true
+fi
+
+if [[ $(command -v gdrive) ]]; then
+    NEED_GDRIVE=true
+fi
+
+if [[ $OS == "Darwin" ]]; then
+    installDbx $DBX_OSX_LINK
+    installGdrive $GDRIVE_OSX_LINK
+else
+    installDbx $DBX_LINUX_LINK
+    installGdrive $GDRIVE_LINUX_LINK
+fi
+
+## Setup firebase
+curl -sL firebase.tools | bash
+
